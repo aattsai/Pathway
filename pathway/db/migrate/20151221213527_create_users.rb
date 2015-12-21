d6bc9bf8672d4447627c7766ba2da90db9438bcd
Aaron Tsai & Jack Huang <pair+aattsai.jhack32@devbootcamp.com>
2015-12-21 17:05:30 -0500

    Change db password to passsword_digest

--- pathway/db/migrate/20151221213527_create_users.rb
+++ pathway/db/migrate/20151221213527_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :first_name, null: false
       t.string :last_name, null: false
       t.string :email, null: false
-      t.string :password, null: false
+      t.string :password_digest, null: false
       t.string :institution, null: false
       t.string :title, null: false
 
