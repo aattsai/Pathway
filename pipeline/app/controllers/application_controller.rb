abd13f3a6ad6ad1243e4aa4682f036b5dd318005
Jack Huang <Jack7100@gmail.com>
2016-02-03 12:56:49 -0500

    Add response to json objs

--- pipeline/app/controllers/application_controller.rb
+++ pipeline/app/controllers/application_controller.rb
@@ -3,4 +3,6 @@ class ApplicationController < ActionController::API
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
   include ActionController::Serialization
+
+  respond_to :json
 end
