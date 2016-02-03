3f24bef0298ec93f32ccc65ee30c7eccb3ddda0d
Jack Huang <Jack7100@gmail.com>
2016-02-03 13:21:56 -0500

    Add routes for user login/register

--- pipeline/config/routes.rb
+++ pipeline/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, only: []
+
+  namespace :user, defaults: { format: :json } do
+    resource :login, only: [:create], controller: :sessions
+    resource :register, only: [:create], controller: :users
+  end
 end
