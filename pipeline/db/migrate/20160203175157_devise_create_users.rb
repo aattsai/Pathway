23d7fbbbe75f3af9aa4780666b618d0f3c1455a6
Jack Huang <Jack7100@gmail.com>
2016-02-03 12:58:49 -0500

    Remove user table, add mandatory fields for devise users table

--- pipeline/db/migrate/20160203175157_devise_create_users.rb
+++ pipeline/db/migrate/20160203175157_devise_create_users.rb
@@ -30,8 +30,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :first_name
-      t.string :last_name
+      t.string :first_name, null: false
+      t.string :last_name, null: false
       t.integer :score
 
       t.timestamps null: false
