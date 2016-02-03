4f42a47023a060fb4ac0407ebaa086708f47e9c2
Jack Huang <Jack7100@gmail.com>
2016-02-03 18:17:44 -0500

    Add access_token string field

--- pipeline/db/schema.rb
+++ pipeline/db/schema.rb
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20160203175157) do
     t.inet     "last_sign_in_ip"
     t.string   "first_name",                          null: false
     t.string   "last_name",                           null: false
+    t.string   "access_token"
     t.integer  "score"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
