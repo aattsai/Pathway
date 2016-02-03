848b6363bebd4110aa31937aad831911e24c55ac
Jack Huang <Jack7100@gmail.com>
2016-02-03 18:19:24 -0500

    Change namespace from user to api

--- pipeline/config/routes.rb
+++ pipeline/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, only: []
 
-  namespace :user, defaults: { format: :json } do
+  namespace :api, defaults: { format: :json } do
     resource :login, only: [:create], controller: :sessions
     resource :register, only: [:create], controller: :users
   end
+  resource :user, only: [:new]
 end
