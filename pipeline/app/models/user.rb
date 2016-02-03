c0cc8b7a6cc16a33fbbf2312f32686bcdb4c1414
Jack Huang <Jack7100@gmail.com>
2016-02-03 18:20:10 -0500

    Add private method and after_create

--- pipeline/app/models/user.rb
+++ pipeline/app/models/user.rb
@@ -8,4 +8,12 @@ class User < ActiveRecord::Base
   has_many :pathways
 
   validates :email, :first_name, :last_name, presence: true
+  after_create :update_access_token!  
+
+  private
+  def update_access_token!
+    self.access_token = "#{self.id}:#{Devise.friendly_token}"
+    save
+  end
+
 end
