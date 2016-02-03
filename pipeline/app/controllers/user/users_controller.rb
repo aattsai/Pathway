408210fe436f9bd59afbb1f50ae038f213988e4b
Jack Huang <Jack7100@gmail.com>
2016-02-03 13:40:50 -0500

    Add controllers and methods for user login/create

---
+++ pipeline/app/controllers/user/users_controller.rb
@@ -0,0 +1,23 @@
+module User
+  class UsersController < ApplicationController
+    skip_before_action :authenticate_user_from_token!, only: [:create]
+
+    # POST /user/login
+    # Creates an user
+    def create
+      @user = User.new user_params
+
+      if @user.save
+        render json: @user, serializer: User::SessionSerializer, root: nil
+      else
+        render json: { error: t('user_create_error') }, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+  end
+end
