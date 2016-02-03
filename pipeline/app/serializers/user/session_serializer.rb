0be7d6bdd0f9d1c3a5e7459b7e094a0a05cea7cb
Jack Huang <Jack7100@gmail.com>
2016-02-03 13:41:48 -0500

    Add serializer for session

---
+++ pipeline/app/serializers/user/session_serializer.rb
@@ -0,0 +1,14 @@
+module User
+  class SessionSerializer < ActiveModel::Serializer
+
+    attributes :email, :token_type, :user_id, :access_token
+
+    def user_id
+      object.id
+    end
+
+    def token_type
+      'Bearer'
+    end
+  end
+end
