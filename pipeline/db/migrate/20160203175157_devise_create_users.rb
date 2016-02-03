5830e8498b3304dc8cc3a11ea815d0bf3d7d197a
Jack Huang <Jack7100@gmail.com>
2016-02-03 12:57:56 -0500

    Add extra fields to user table

--- pipeline/db/migrate/20160203175157_devise_create_users.rb
+++ pipeline/db/migrate/20160203175157_devise_create_users.rb
@@ -30,6 +30,9 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :first_name
+      t.string :last_name
+      t.integer :score
 
       t.timestamps null: false
     end
