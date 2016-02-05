a89a45f6ca8a95341d1256ceafaa32dcfeffbdbb
Jack Huang <Jack7100@gmail.com>
2016-02-04 19:35:15 -0500

    Add logout function and change error message for invalid login attempt

--- pipeline/app/controllers/api/sessions_controller.rb
+++ pipeline/app/controllers/api/sessions_controller.rb
@@ -15,11 +15,16 @@ module Api
       end
     end
 
+    def destroy
+      warden.logout
+      render json: {} 
+    end
+
     private
 
     def invalid_login_attempt
       warden.custom_failure!
-      render json: {error: t('sessions_controller.invalid_login_attempt')}, status: :unprocessable_entity
+      render json: {error: ('Incorrect username/password')}, status: :unprocessable_entity
     end
 
   end
