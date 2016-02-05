c22068f8394555afb78ff2fbbf83c2461c399849
Jack Huang <Jack7100@gmail.com>
2016-02-04 19:55:59 -0500

    Changed error message to show user error messages, and remove password_confirmation field

--- pipeline/app/controllers/api/users_controller.rb
+++ pipeline/app/controllers/api/users_controller.rb
@@ -2,22 +2,21 @@ module Api
   class UsersController < ApplicationController
     skip_before_action :authenticate_user_from_token!, only: [:create]
 
-    # POST /user/login
+    # POST /api/register
     # Creates an user
     def create
       @user = User.new user_params
-
       if @user.save
-        render json: @user, serializer: API::SessionSerializer, root: nil
+        render json: @user, serializer: Api::SessionSerializer, root: nil
       else
-        render json: { error: t('user_create_error') }, status: :unprocessable_entity
+        render json: { error: @user.errors.full_messages }, status: :unprocessable_entity
       end
     end
 
     private
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
     end
   end
 end
