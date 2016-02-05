4f2441e490d7b05b7e013bd89db16b35f33bcb19
Jack Huang <Jack7100@gmail.com>
2016-02-04 19:03:33 -0500

    Add destroy route

--- pipeline/config/routes.rb
+++ pipeline/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, only: []
 
   namespace :api, defaults: { format: :json } do
-    resource :login, only: [:create], controller: :sessions
+    resource :login, only: [:create, :destroy], controller: :sessions
     resource :register, only: [:create], controller: :users
   end
   resource :user, only: [:new]
