0e33fa403910d0891a3bc9c77e7101582a86a290
Jack Huang <Jack7100@gmail.com>
2016-03-02 09:40:53 -0500

    Add route for users and teams

--- pipeline/config/application.rb
+++ pipeline/config/application.rb
@@ -17,6 +17,9 @@ Bundler.require(*Rails.groups)
 
 module Pipeline
   class Application < Rails::Application
+    # Use the responders controller from the responders gem
+    config.app_generators.scaffold_controller :responders_controller
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -31,5 +34,8 @@ module Pipeline
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.to_prepare do
+      DeviseController.respond_to :html, :json
+    end
   end
 end
--- pipeline/config/routes.rb
+++ pipeline/config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root 'application#index'
   # get '*path' => 'application#index'
-  # devise_for :users
-  # devise_for :teams
+  devise_for :users, defaults: { format: :json }
+  devise_for :teams, defaults: { format: :json }
 
   namespace :api, defaults: { format: :json } do
-    resources :login, only: [:create, :destroy], controller: :sessions
-    resources :researcher, only: [:create, :destroy], controller: :researchers
+    # resources :login, only: [:create, :destroy], controller: :sessions
+    # resources :researcher, only: [:create, :destroy], controller: :researchers
     resources :register, only: [:create], controller: :users
     resources :pathways, only: [:index, :show] do 
       resources :projects, only:[:show]
