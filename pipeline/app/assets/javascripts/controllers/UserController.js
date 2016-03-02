284e615ca7de2d17d3f5ba9d99ef0ba12d1cdc7f
Jack Huang <Jack7100@gmail.com>
2016-03-02 11:17:18 -0500

    Fix login issues

--- pipeline/app/assets/javascripts/controllers/UserController.js
+++ pipeline/app/assets/javascripts/controllers/UserController.js
@@ -11,52 +11,6 @@ pathwayApp.controller('UserController',
       return sessionStorage.researcher == "true" ? true : false
     }
     $scope.login = function() {
-      $http({method: 'POST',
-             url: '/api/login',
-             data: {username: $scope.user.email, password: $scope.user.password},
-             success_message: "You have been logged in.",
-             error_entity: $scope.login_error
-       }).success(function(data, status){
-        sessionStorage.access_token = data.access_token
-        sessionStorage.email = data.email
-        $scope.userId = data.user_id
-        $scope.message = "You have been logged in."
-        window.location.href = '/'
-       }).error(function(data, status){
-        $scope.message = data.error
-       })
-  };
-
-  $scope.logout = function() {
-      $http({method: 'DELETE',
-              url: '/api/login/'+ $scope.userId
-  // $scope.researchLogin = function() {
-  //     $http({method: 'POST', 
-  //            url: '/teams/sign_in',
-  //            data: {email: $scope.researcher.email, password: $scope.researcher.password},
-  //            error_entity: $scope.login_error
-  //      }).success(function(data, status){
-  //       console.log("hello from research login")
-  //      }).error(function(data, status){
-  //       $scope.message = data.error
-  //      })
-  // };
-  $scope.researchLogin = function() {
-      $http({method: 'POST', 
-             url: '/api/researcher',
-             data: {username: $scope.researcher.email, password: $scope.researcher.password},
-             error_entity: $scope.login_error
-       }).success(function(data, status){
-        sessionStorage.access_token = data.access_token
-        sessionStorage.email = data.email
-        sessionStorage.researcher = true
-        $scope.researcherId = data.user_id
-        $scope.message = "You have been logged in."
-        window.location.href = '/'
-       }).error(function(data, status){
-        $scope.message = data.error
-       })
-  };
       var credentials = {
         email: $scope.user.email,
         password: $scope.user.password
