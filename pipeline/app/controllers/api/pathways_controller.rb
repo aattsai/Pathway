97eb30ec3985763c1eedc50144b286c049abf959
Jack Huang <Jack7100@gmail.com>
2016-03-07 11:47:51 -0500

    Add creating pathway functionality

--- pipeline/app/controllers/api/pathways_controller.rb
+++ pipeline/app/controllers/api/pathways_controller.rb
@@ -8,5 +8,21 @@ module Api
       pathway = Pathway.find(params[:id])
       render json: pathway, include: [pathway_projects: {include: :project } ]
     end
+
+    def create
+      user = User.find(params[:user])
+      pathway = user.pathways.new(pathway_params)
+      if pathway.save
+        render json: pathway
+      else
+        render json: 'Error saving pathway'
+      end
+    end
+
+    private
+
+    def pathway_params
+      params.require(:pathway).permit(:name, :description)
+    end
   end
 end
