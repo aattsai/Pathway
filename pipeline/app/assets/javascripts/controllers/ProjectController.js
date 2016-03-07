5336da95d90da7923520f93369335e7e85602642
Jack Huang <Jack7100@gmail.com>
2016-03-07 16:23:09 -0500

    Add functionality to redirect once 10 projects are added to the pathway

--- pipeline/app/assets/javascripts/controllers/ProjectController.js
+++ pipeline/app/assets/javascripts/controllers/ProjectController.js
@@ -23,6 +23,7 @@ angular.module('pathwayApp')
       console.log(data)
     })
   }
+  var projects = 0
   var currentPathway = 0
   $scope.projectsAdded = []
   $scope.addProject = function(project, donated_weight) {
@@ -31,8 +32,10 @@ angular.module('pathwayApp')
       url: '/api/pathways/' + $stateParams.id + '/projects',
       data: {projects: project, weight: donated_weight } 
     }).success(function(data, status){
+      projects += 1
       currentPathway = data.pathway_id
       $scope.projectsAdded.push(projectObj.name)
+      if (projects == 10) {$state.go('showPathway', {'id': currentPathway })} 
     }).error(function(data){
       console.log("Error adding project")
     })
