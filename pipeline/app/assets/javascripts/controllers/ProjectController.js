d71bcd4f8c2c46992737c5154c7391954167338d
Jack Huang <Jack7100@gmail.com>
2016-03-07 14:25:43 -0500

    Create controller functions for adding the pathway-project

--- pipeline/app/assets/javascripts/controllers/ProjectController.js
+++ pipeline/app/assets/javascripts/controllers/ProjectController.js
@@ -1,18 +1,32 @@
 'use strict'
 
 angular.module('pathwayApp')
-.controller('ProjectController', function ($scope, $http, $stateParams) {
+.controller('ProjectController', function ($scope, $http, $stateParams, $state) {
   $http({method: 'GET',
          url: '/api/pathways/' + $stateParams.pathway_id + '/projects/' + $stateParams.id
   }).success(function(data, status){
     $scope.project = data
   })
-
+  $scope.projects = $http({method: 'GET',
+         url: '/api/projects'
+  }).success(function(data, status){
+    $scope.allProjects = data
+  })
   $scope.create = function() {
-    debugger
     $http({method: 'POST',
       url: '/api/projects',
       data: {project: $scope.project } 
+    }).success(function(data, status){
+      console.log("hello from create()")
+      $state.go('home')
+    }).error(function(data){
+      console.log(data)
+    })
+  }
+  $scope.addProject = function(project, donated_weight) {
+    $http({method: 'POST',
+      url: '/api/pathways/' + $stateParams.id + '/projects',
+      data: {projects: project, weight: donated_weight } 
     }).success(function(data, status){
       console.log("hello from create()")
       $state.go('home')
